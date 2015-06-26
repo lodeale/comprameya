128d12d50b2699ee5c039f695b0a0b1225fc6a02
lodeale <lodeale@gmail.com>
2015-06-26 14:52:12 -0300

    Se genero nuevas tablas para la base de datos comprameya

--- comprameya.sql
+++ comprameya.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 29-05-2015 a las 17:49:20
+-- Tiempo de generación: 26-06-2015 a las 14:51:12
 -- Versión del servidor: 5.5.43
 -- Versión de PHP: 5.4.39-0+deb7u2
 
@@ -28,9 +28,68 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `categorias` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(50) NOT NULL,
+  `categoria` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `categoria`) VALUES
+(1, 'Zapatos'),
+(2, 'Remeras'),
+(3, 'Pantalones'),
+(4, 'Joyeria'),
+(5, 'Abrigos');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pais`
+--
+
+CREATE TABLE IF NOT EXISTS `pais` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pais` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `pais`
+--
+
+INSERT INTO `pais` (`id`, `pais`) VALUES
+(1, 'Argentina');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+CREATE TABLE IF NOT EXISTS `personas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `cel` varchar(20) NOT NULL,
+  `direccion` varchar(150) NOT NULL,
+  `id_provincia` int(11) NOT NULL,
+  `codigo_postal` int(4) NOT NULL,
+  `dni` int(8) NOT NULL,
+  `edad` int(3) NOT NULL,
+  `sexo` char(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_provincia` (`id_provincia`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Volcado de datos para la tabla `personas`
+--
+
+INSERT INTO `personas` (`id`, `nombre`, `apellido`, `cel`, `direccion`, `id_provincia`, `codigo_postal`, `dni`, `edad`, `sexo`) VALUES
+(1, 'tito', 'Nacamura', '12123123123', 'Mz 44 pc 12 barrio guiraldes', 1, 3500, 31654987, 23, 'M'),
+(3, 'Elvira', 'Gimenes', '343453454', 'Cordoba 234', 2, 3400, 12123123, 23, 'F');
 
 -- --------------------------------------------------------
 
@@ -40,14 +99,45 @@ CREATE TABLE IF NOT EXISTS `categorias` (
 
 CREATE TABLE IF NOT EXISTS `productos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `producto` varchar(250) NOT NULL,
-  `cantidad` int(7) NOT NULL,
-  `precio` decimal(7,2) NOT NULL,
-  `imagen` varchar(150) NOT NULL,
+  `descripcion` text NOT NULL,
+  `resumen` varchar(100) NOT NULL,
   `id_categoria` int(11) NOT NULL,
+  `fecha_creado` date NOT NULL,
+  `cantidad` int(7) NOT NULL,
+  `precio` double(8,2) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_categoria` (`id_categoria`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `descripcion`, `resumen`, `id_categoria`, `fecha_creado`, `cantidad`, `precio`) VALUES
+(1, 'Baqueros M/S blancos', 'Baqueros M/S blancos', 3, '2015-06-26', 23, 250.00),
+(2, 'Camperas,xl,marrones,con capucha', '', 5, '2015-06-26', 12, 560.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provincias`
+--
+
+CREATE TABLE IF NOT EXISTS `provincias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `provincia` varchar(20) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `id_pais` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_pais` (`id_pais`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Volcado de datos para la tabla `provincias`
+--
+
+INSERT INTO `provincias` (`id`, `provincia`, `id_pais`) VALUES
+(1, 'Chaco', 1),
+(2, 'Corrientes', 1);
 
 -- --------------------------------------------------------
 
@@ -57,22 +147,68 @@ CREATE TABLE IF NOT EXISTS `productos` (
 
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario` varchar(20) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
-  `clave` varchar(120) NOT NULL,
-  `privilegio` int(2) NOT NULL,
-  `token` varchar(200) NOT NULL,
+  `id_persona` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `clave` varchar(150) NOT NULL,
+  `fecha_ingreso` date NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_persona` (`id_persona`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `id_persona`, `email`, `clave`, `fecha_ingreso`, `activo`) VALUES
+(2, 1, 'tito@nacamura.com', '8cb2237d0679ca88db6464eac60da96345513964', '2015-06-26', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE IF NOT EXISTS `ventas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_producto` int(11) NOT NULL,
+  `id_personas` int(11) NOT NULL,
+  `fecha_venta` date NOT NULL,
+  `email_contacto` varchar(20) NOT NULL,
+  `total` decimal(8,2) NOT NULL,
+  `pagado` decimal(8,2) NOT NULL,
+  `fecha_ultimo_pago` date NOT NULL,
+  `forma_pago` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `personas`
+--
+ALTER TABLE `personas`
+  ADD CONSTRAINT `personas_ibfk_1` FOREIGN KEY (`id_provincia`) REFERENCES `provincias` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`);
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `provincias`
+--
+ALTER TABLE `provincias`
+  ADD CONSTRAINT `provincias_ibfk_1` FOREIGN KEY (`id_pais`) REFERENCES `pais` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_3` FOREIGN KEY (`id_persona`) REFERENCES `personas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
