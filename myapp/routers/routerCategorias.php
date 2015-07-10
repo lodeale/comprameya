4b96e94a3f08573a370bab435ae5afd46fe3f00f
veronica Tribbia <verotribb@gmail.com>
2015-07-10 19:32:21 -0300

    Metodo delCategoria

--- myapp/routers/routerCategorias.php
+++ myapp/routers/routerCategorias.php
@@ -7,4 +7,12 @@ $app->get('/buscarCategoria(/(:valor))',function($valor=false) use ($db) {
 
 });
 
- ?>
+$app->get('/delCategoria/:idp', function($idp) use ($db){
+    try {
+        $sql = "DELETE FROM categorias WHERE categoria = ?";
+        $delete = $db->getInstance()->consultar($sql,array($idp));
+        echo json_encode($delete);
+    } catch (Exception $e) {
+        echo json_encode(array("Error"=>$e,"Message"=>"No se pudo borrar el elemento"));
+    }
+});
